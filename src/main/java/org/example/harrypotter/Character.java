9b62f865ffce69f9adc7118ab7f4a3c05576378d
Débora <debora.rubio.soliva@gmail.com>
2025-05-16 14:03:13 +0200

    add some more code

--- src/main/java/org/example/harrypotter/Character.java
+++ src/main/java/org/example/harrypotter/Character.java
@@ -9,21 +9,27 @@ public class Character {
         this.house = house;
         this.age = age;
     }
+
     public String getName() {
         return name;
     }
+
     public void setName(String name) {
         this.name = name;
     }
+
     public String getHouse() {
         return house;
     }
+
     public void setHouse(String house) {
         this.house = house;
     }
+
     public int getAge() {
         return age;
     }
+
     public void setAge(int age) {
         this.age = age;
     }
