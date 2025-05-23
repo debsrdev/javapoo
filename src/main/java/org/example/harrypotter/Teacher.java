05e3373606da919a290e2549b89b79bdbac22060
Débora <debora.rubio.soliva@gmail.com>
2025-05-23 15:22:50 +0200

    feature(refactor) refactor learnedSpells and castSpells into Character

--- src/main/java/org/example/harrypotter/Teacher.java
+++ src/main/java/org/example/harrypotter/Teacher.java
@@ -5,12 +5,10 @@ import java.util.List;
 
 public class Teacher extends Character {
     private String subject;
-    private List<String> learnedSpells;
 
     public Teacher (String name, String house, int age, String subject) {
         super(name, house, age);
         this.subject = subject;
-        this.learnedSpells = new ArrayList<>();
     }
 
     public String getSubject() {
@@ -28,26 +26,5 @@ public class Teacher extends Character {
     @Override
     public void introduce() {
         System.out.println("Hi, I'm professor " + getName() + " and I teach " + subject);
-    }
-
-    @Override
-    public void castSpell(String spell) {
-        System.out.println(getName() + " casts " + spell);
-    }
-    @Override
-    public void learnSpell(String spell) {
-        learnedSpells.add(spell);
-        System.out.println(getName() + " knows how to cast " + spell);
-    }
-    @Override
-    public void listSpells() {
-        System.out.println(getName() + "'s learned spells: ");
-        if (learnedSpells.isEmpty()) {
-            System.out.println(" - No spells learned yet");
-        } else {
-            for (String spell : learnedSpells) {
-                System.out.println(" - " + spell);
-            }
-        }
     }
 }
